884646e7d09e8ad52f0f15572c58026a13884db7
bAleksiejczuk <106964945+bAleksiejczuk@users.noreply.github.com>
2025-04-12 18:58:52 +0200

    email get update

--- src/main/java/com/example/baleksiejczuk/User.java
+++ src/main/java/com/example/baleksiejczuk/User.java
@@ -3,12 +3,15 @@ package com.example.baleksiejczuk;
 public class User {
     String name;
     String surname;
+
+    String email;
     String login;
     String password;
 
-    public User(String name, String surname, String login, String password ){
+    public User(String name, String surname, String email , String login, String password ){
         this.name = name;
         this.surname = surname;
+        this.email = email;
         this.login = login;
         this.password = password;
     }
@@ -20,6 +23,8 @@ public class User {
     public String getSurname() {
         return surname;
     }
+
+    public String getEmail(){return email; }
 
     public String getLogin() {
         return login;
