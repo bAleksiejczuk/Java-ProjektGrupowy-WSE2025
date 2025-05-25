2bd86a46073cf7506d508bed40b2d16fd77a310b
bAleksiejczuk <106964945+bAleksiejczuk@users.noreply.github.com>
2025-05-25 16:14:39 +0200

    save to session links when user login

---
+++ src/main/java/com/example/baleksiejczuk/LinkData.java
@@ -0,0 +1,21 @@
+package com.example.baleksiejczuk;
+
+public class LinkData {
+    private String url;
+    private String name;
+    private boolean isPrivate;
+    private String user;
+    private String addedAt;
+    private String addedTime;
+    private String category;
+    private int likes;
+
+    public String getUrl() { return url; }
+    public String getName() { return name; }
+    public boolean isPrivate() { return isPrivate; }
+    public String getUser() { return user; }
+    public String getAddedAt() { return addedAt; }
+    public String getAddedTime() { return addedTime; }
+    public String getCategory() { return category; }
+    public int getLikes() { return likes; }
+}
