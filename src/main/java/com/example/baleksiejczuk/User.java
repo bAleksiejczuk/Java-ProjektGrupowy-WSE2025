de469f3ae1ddccb3c923b33e1a52e81678baa879
bAleksiejczuk <106964945+bAleksiejczuk@users.noreply.github.com>
2025-05-25 16:13:55 +0200

    save to session links when user login

--- src/main/java/com/example/baleksiejczuk/User.java
+++ src/main/java/com/example/baleksiejczuk/User.java
@@ -1,41 +1,19 @@
-package com.example.baleksiejczuk;
+    package com.example.baleksiejczuk;
 
-public class User {
-//    String name;
-//    String surname;
-//
-//    String email;
-    String login;
-    String password;
+    public class User {
+        private String login;
+        private String password;
 
-//    public User(String name, String surname, String email , String login, String password ){
-public User(String login, String password ){
+        public User(String login, String password) {
+            this.login = login;
+            this.password = password;
+        }
 
-//        this.name = name;
-//        this.surname = surname;
-//        this.email = email;
-        this.login = login;
-        this.password = password;
+        public String getLogin() {
+            return login;
+        }
+
+        public String getPassword() {
+            return password;
+        }
     }
-
-//    public String getName(){
-//        return name;
-//    }
-//
-//    public String getSurname() {
-//        return surname;
-//    }
-//
-//    public String getEmail(){return email; }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-
-}
-
