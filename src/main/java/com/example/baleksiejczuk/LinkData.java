0256d5c921fa47c781535d12bc103776be325fa7
bAleksiejczuk <106964945+bAleksiejczuk@users.noreply.github.com>
2025-05-26 15:49:27 +0200

    add link to session

--- src/main/java/com/example/baleksiejczuk/LinkData.java
+++ src/main/java/com/example/baleksiejczuk/LinkData.java
@@ -18,4 +18,16 @@ public class LinkData {
     public String getAddedTime() { return addedTime; }
     public String getCategory() { return category; }
     public int getLikes() { return likes; }
+
+
+    public void setUrl(String url) { this.url = url; }
+    public void setName(String name) { this.name = name; }
+    public void setPrivate(boolean isPrivate) { this.isPrivate = isPrivate; }
+    public void setUser(String user) { this.user = user; }
+    public void setAddedAt(String addedAt) { this.addedAt = addedAt; }
+    public void setAddedTime(String addedTime) { this.addedTime = addedTime; }
+    public void setCategory(String category) { this.category = category; }
+    public void setLikes(int likes) { this.likes = likes; }
+
+
 }
